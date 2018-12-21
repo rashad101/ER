fd4bb86cd1445c31fcc97655592745f37ea90172
Rashad Al Hasan Rony <rah.rony@gmail.com>
2018-12-21 13:11:43 +0100

    added test_lcquad() method

--- models/ER_Test.py
+++ models/ER_Test.py
@@ -23,15 +23,21 @@ print("Loaded model from disk")
 
 
 max_text_length = 500
-test_input = ["licensor","teleserial","james william whilt"]
-#                 R          R                  E
+test_input = ["licensor","teleserial","james william whilt","recentemente","sugar sweetened", "Rivers", "writes"]
+#                 R          R                  E              R               R               R        R
 
-
-
+'''
 for word in test_input:
+    found = True
     tmp = []
     for w in word.split(" "):
-        tmp.append(word_to_id[w])
+        if w in word_to_id:
+            tmp.append(word_to_id[w])
+        else:
+            found = False
+
+    if found==False:
+        continue
     tmp_padded = sequence.pad_sequences([tmp], maxlen=max_text_length)
     #loaded_model.compile(loss='binary_crossentropy',optimizer='adam', metrics=['accuracy'])
     prediction = loaded_model.predict(np.array([tmp_padded][0]))[0][0]
@@ -41,3 +47,74 @@ for word in test_input:
         print(word+" -> Entity:{}%%".format(prediction))
 
     #print(loaded_model.predict_classes(np.array([tmp_padded][0])))
+'''
+
+
+def test_lcquad():
+
+    prediction_stat = []
+    threshold = 0.9975
+    accuracy = 0.0
+    with open('../data/Test_LC_QuAD.csv',encoding="utf-8") as f:
+        rows = csv.reader(f, delimiter=',')
+        correct = 0
+        count = 0
+        write_to_file = []
+        for er in rows:
+            found = True
+            tmp = []
+
+            er_word = er[0]
+            er_type = er[1]
+
+            for w in er_word.split(" "):
+                if w in word_to_id:
+                    tmp.append(word_to_id[w])
+                else:
+                    found = False
+
+            if found == False:
+                continue
+
+            pred = ""
+            prob = 0.0
+
+            tmp_padded = sequence.pad_sequences([tmp], maxlen=max_text_length)
+            prediction = loaded_model.predict(np.array([tmp_padded][0]))[0][0]
+
+            if prediction < threshold:
+                #print(word + " -> Relation:{}%%".format(1 - prediction))
+                prob = (1-prediction)
+                pred ="R"
+            else:
+                #print(word + " -> Entity:{}%%".format(prediction))
+                prob= prediction
+                pred = "E"
+
+            dict = {
+                "ER_word": er_word,
+                "actual_type": er_type,
+                "predicted_type": pred,
+                "probability_of_being_entity": prob
+            }
+            prediction_stat.append(dict)
+            write_to_file.append([er_word,er_type,pred,prob])   # Writing these information to the file
+
+            if er_type==pred:
+                correct+=1
+            count+=1
+
+            accuracy = (correct/count)*100
+
+        to_write = open("../data/threshold__"+str(threshold)+"acc__"+str(accuracy)+".csv", "w")
+        data_writer = csv.writer(to_write)
+        data_writer.writerows(write_to_file)
+        print( accuracy)
+
+
+
+test_lcquad()
+
+
+
+
