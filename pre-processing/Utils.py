85fb3032761fa74fb88557ab669b91a8dfed60a6
Rashad Al Hasan Rony <rah.rony@gmail.com>
2019-01-23 02:15:05 +0100

    Add files via upload

--- pre-processing/Utils.py
+++ pre-processing/Utils.py
@@ -6,8 +6,11 @@ from nltk.tokenize import WordPunctTokenizer
 import numpy as np
 import nltk
 import re
+import csv
 import heapq
 from SPARQLWrapper import SPARQLWrapper, JSON
+
+
 
 #returns a list of triple-list
 def load_data():
@@ -26,8 +29,8 @@ def load_data():
                 predicate = predicate_uri[predicate_uri.rfind("/")+1:]
 
                 triple_list.append([subject,predicate,object_])
-
         return triple_list
+
 
 
 def count_ontology_classes():
@@ -83,7 +86,7 @@ def write_dbpedia_relations():
                     all_relations_with_label[uri_rel] = []
                     all_relations_with_label[uri_rel].append(label)
 
-
+    #print(len(all_relations_with_label))
     with open('../data/all_relations_with_label.json', 'w') as outfile:
         json.dump([all_relations_with_label], outfile)
     write_to_file.close()
@@ -99,6 +102,7 @@ def load_DBpedia_relations():
     with open('../data/all_relations_with_label.json') as f:
         relations = json.load(f)
         relations = relations[0]
+        print(len(relations))
         return relations
 
 def build_data():
@@ -133,7 +137,7 @@ def build_data():
             relation_set.add(rel['rel'])
 
 
-        # get top 5 relations related to the entity from dbpedia relations
+        # get top 5 relations related to the entity from dbpedia relations which are extracted from dbpedia relations
         relations_for_entity = get_relations(sub)
 
         similarity = []
@@ -141,10 +145,16 @@ def build_data():
             if rel!=pred:
                 similarity.append({"rel":rel, "similarity": textdistance.cosine(rel,pred)})
 
-        top5_er = heapq.nlargest(5,similarity, key= lambda s: s['similarity'])
+        top5_er = heapq.nlargest(len(similarity),similarity, key= lambda s: s['similarity'])
 
+
+        taken =0
+        blacklist = ["wikiPageDisambiguates","wikiPageWikiLink","wikiPageRedirects"]
         for rel in top5_er:
-            relation_set.add(rel['rel'])
+            if taken<5 and rel['rel'] not in blacklist:
+                relation_set.add(rel['rel'])
+                taken+=1
+
 
         # make a set ( 1 exact relation + 5 similar relations from dbpedia + 5  similar relations from dbpedia for that entity)
         d = {}
@@ -154,26 +164,21 @@ def build_data():
 
         er_n_list.append(d)
         j+=1
-        if j%30==0:
+        if j%50==0:
             percent = (j/total)*100.0
             print("done %.2f%%"%percent)
 
-    with open('../data/ER_n_relList.json', 'w') as outfile:
-        json.dump([er_n_list], outfile)
+    with open('../data/ER_n_relList_v2.json', 'w') as outfile:
+        json.dump([er_n_list], outfile, indent=4)
 
 
 def get_relations(entity):
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    entity = remove_punct(entity)
-    entity = "_".join(entity.split(" "))
-    entity = entity.replace("__","_")
-    if entity[len(entity)-1]=="_":
-        entity= entity[0:len(entity)-1]
-    #print("entity is :  {}".format(entity))
-    sparql.setQuery("SELECT distinct ?rel where { ?obj ?rel dbr:"+entity+"}")
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
 
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery("SELECT distinct ?rel where { ?obj ?rel <http://dbpedia.org/resource/"+entity+">}")
+    sparql.setReturnFormat(JSON)
+
+    results = sparql.query().convert()
     results = results['results']['bindings']
     link_set = set()
     for data in results:
@@ -195,4 +200,41 @@ def remove_punct(sentence, keep_apostrophe = False):
 
 
 
+def build_lcquad_test():
+    all_er = {}
+
+    e=0
+    r=0
+    with open('../data/FullyAnnotated_LCQuAD5000.json') as f:
+        data = json.load(f)
+        for d in data:
+            e_mapping = d["entity mapping"]
+            for ent in e_mapping:
+                all_er[ent["label"]]="E"
+                e+=1
+            pd_mapping = d["predicate mapping"]
+            for pd in pd_mapping:
+                if "label" in pd:
+                    all_er[pd["label"]]="R"
+                    r+=1
+
+        print("Total entities: {}".format(e))
+        print("Total relations: {}".format(r))
+
+    rows = []
+    for k,v in all_er.items():
+        rows.append([k,v])
+
+    to_write = open("../data/Test_LC_QuAD.csv","w")
+    data_writer = csv.writer(to_write)
+    data_writer.writerows(rows)
+
+
+#count_ontology_classes()
+#write_dbpedia_relations()
 build_data()
+#get_relations("The_Invisible_(band)")
+#build_lcquad_test()
+#load_DBpedia_relations()
+
+#The_Invisible_(band)
